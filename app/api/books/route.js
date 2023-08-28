be1e24fe1e5c8af0c9f86291f1c1c871656db45a
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-08-28 09:40:37 +0900

    230828 2-20_Adding a New Book

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -5,3 +5,20 @@ import { NextResponse } from "next/server"; //가져오기 요청
 export async function GET(req) {
   return NextResponse.json(books); //books를 반환
 }
+
+//새로운 책을 추가하고자함
+export async function POST(req) {
+  //제이슨에서 제목,링크,이미지를 얻을꺼임
+  const { title, link, img } = await req.json();
+
+  const newBook = {
+    id: books.length + 1,
+    title,
+    link,
+    img,
+  };
+
+  books.push(newBook);
+
+  return NextResponse.json("Book added successfully");
+}
