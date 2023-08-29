8812fa6fc49f194b6c056ce3f15ea91eb6e37e6b
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-08-29 10:54:47 +0900

    230829_2-21 Refactor Server to Client Component

--- app/components/Books.jsx
+++ app/components/Books.jsx
@@ -1,17 +1,56 @@
+"use client"; // this is a client component 👈🏽
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import LoadingPage from "../loading";
 
 //data.json에서 추출하는 api경로 핸들러 만들기
 async function getBooks() {
-  const res = await fetch("http://localhost:3000/api/books");
+  const res = await fetch("/api/books"); //
   const json = await res.json();
   return json;
 }
 
-const Books = async () => {
-  const books = await getBooks();
+const Books = () => {
+  // const books = await getBooks();
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    getBooks().then(() => {
+      setBooks(books);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const res = await fetch(`/api/books/search?query=${query}`);
+    const books = await res.json();
+    setBooks(books);
+    setLoading(false);
+  };
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search Books..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
+      </form>
       <h1>Books</h1>
       {books.map((book) => (
         <div>
